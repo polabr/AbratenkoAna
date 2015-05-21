ed28c4f85922ed60a90785a05159dba91159c5f1
David Kaleko <kaleko@nevis.columbia.edu>
2015-05-21 09:47:27 -0500

    filter for simple events, so i can move away from nuance interaction channel requirements

--- NuEnergyStuff/LinkDef.h
+++ NuEnergyStuff/LinkDef.h
@@ -12,7 +12,7 @@
 #pragma link C++ class larlite::NuEnergyReco+;
 #pragma link C++ class larlite::CheckGenieOutput+;
 #pragma link C++ class larlite::util::ECCQECalculator+;
-
+#pragma link C++ class larlite::NpNn1l0elseFilter+;
 //ADD_NEW_CLASS ... do not change this line
 #endif
 
