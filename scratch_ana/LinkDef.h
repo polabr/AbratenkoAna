f67e71dda22ebbb787575f5cc1f3087852979cda
David Kaleko <kaleko@nevis.columbia.edu>
2015-08-24 13:38:35 -0500

    removing cosmic ana because its causing conflict within my other repositories due to root6 bug

--- scratch_ana/LinkDef.h
+++ scratch_ana/LinkDef.h
@@ -10,17 +10,18 @@
 #pragma link off all functions;
 
 //#pragma link C++ class larlite::VerifyShowerLengthFormula+;
-#pragma link C++ class larlite::CosmicAna+;
 #pragma link C++ class larlite::CosmicTrackLister+;
 #pragma link C++ class cluster::SimChannelViewer+;
 #pragma link C++ class larlite::MuShowerRecoStudy+;
 #pragma link C++ class larlite::ShowerRecoStudy+;
 #pragma link C++ class larlite::ProtonStudy+;
 #pragma link C++ class larlite::test_reco_and_mc+;
-#pragma link C++ class larlite::HitViewer+;
+#pragma link C++ class larlite::KalekoHitViewer+;
+#pragma link C++ class larlite::NeutrinoParentInvestigation+;
 //ADD_NEW_CLASS ... do not change this line
 
 #endif
+
 
 
 
