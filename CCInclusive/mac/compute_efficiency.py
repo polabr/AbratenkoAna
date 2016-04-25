c20328e8aa609aeccf3d9f74317ad509534c1bf4
David Kaleko <kaleko@nevis.columbia.edu>
2016-04-25 14:39:28 -0500

    simple script to do ttree draw (pandas) and compute efficiency

---
+++ CCInclusive/mac/compute_efficiency.py
@@ -0,0 +1,23 @@
+import sys
+
+if len(sys.argv) != 2:
+    print "Usage: python %s input_file.root" % sys.argv[0]
+
+infile = sys.argv[1]
+
+issignalsample = 'signal' in infile
+
+import pandas as pd
+from root_numpy import root2array
+
+df = pd.DataFrame( root2array( infile, 'evttree' ) )
+
+cuts = "is_truth_fiducial and is_numuCC and flash_in_bgw and is_areco_vtx_in_fidvol and is_atrack_fromvtx and longest_trk_contained and longest_trk_range_longenough"
+
+nocuts = "is_truth_fiducial and is_numuCC"
+
+num = float(len(df.query(cuts)))
+
+denom = len(df) if not issignalsample else len(df.query(nocuts))
+
+print "Efficiency: %d/%d = %0.2f%%" % (num,denom,100*(num/denom))
