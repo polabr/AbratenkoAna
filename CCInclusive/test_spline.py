38fe0d5e5ba11ec0e7eddf57f752b81309536d38
David Kaleko <kaleko@nevis.columbia.edu>
2016-04-27 13:32:33 -0500

    quick script to test muon momentum spline

---
+++ CCInclusive/test_spline.py
@@ -0,0 +1,16 @@
+from ROOT import larlite as fmwk
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = fmwk.MuTrackMomentumSpline()
+
+xes = np.linspace(1,1000,100)
+ys = np.array([a.GetMuMomentum(poop) for poop in xes])
+plt.plot(xes,ys,'ro--')
+plt.title('Muon Energy based on Range from Spline',fontsize=16)
+plt.xlabel('Muon Range [cm]',fontsize=16)
+plt.ylabel('Muon Kinetic Energy [GeV]',fontsize=16)
+plt.xscale('log')
+plt.grid(True,'both')
+plt.show()
+
