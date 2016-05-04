adaf59d97322b101e0c2b76851ded025d252a017
David Kaleko <kaleko@nevis.columbia.edu>
2016-05-04 13:14:12 -0500

    moving filter into this directory to save on debugging compile errors on nevis machine

--- CCInclusive/LinkDef.h
+++ CCInclusive/LinkDef.h
@@ -17,6 +17,7 @@
 #pragma link C++ class larlite::EventSelector+;
 #pragma link C++ class larlite::TestKinematicNuReco+;
 #pragma link C++ class larlite::XiaoEventSelector+;
+#pragma link C++ class larlite::MC_1mu1pNn0else_Filter+;
 //ADD_NEW_CLASS ... do not change this line
 #endif
 
---
+++ CCInclusive/MC_1mu1pNn0else_Filter.h
@@ -0,0 +1,68 @@
+/**
+ * \file MC_1mu1pNn0else_Filter.h
+ *
+ * \ingroup EventFilters
+ * 
+ * \brief Class def header for a class MC_1mu1pNn0else_Filter
+ *
+ * @author davidkaleko
+ */
+
+/** \addtogroup EventFilters
+
+    @{*/
+
+#ifndef LARLITE_MC_1MU1PNN0ELSE_FILTER_H
+#define LARLITE_MC_1MU1PNN0ELSE_FILTER_H
+
+#include "Analysis/ana_base.h"
+
+namespace larlite {
+  /**
+     \class MC_1mu1pNn0else_Filter
+     User custom analysis class made by SHELL_USER_NAME
+   */
+  class MC_1mu1pNn0else_Filter : public ana_base{
+  
+  public:
+
+    /// Default constructor
+    MC_1mu1pNn0else_Filter(){ _name="MC_1mu1pNn0else_Filter"; _fout=0;}
+
+    /// Default destructor
+    virtual ~MC_1mu1pNn0else_Filter(){}
+
+    /** IMPLEMENT in MC_1mu1pNn0else_Filter.cc!
+        Initialization method to be called before the analysis event loop.
+    */ 
+    virtual bool initialize();
+
+    /** IMPLEMENT in MC_1mu1pNn0else_Filter.cc! 
+        Analyze a data event-by-event  
+    */
+    virtual bool analyze(storage_manager* storage);
+
+    /** IMPLEMENT in MC_1mu1pNn0else_Filter.cc! 
+        Finalize method to be called after all events processed.
+    */
+    virtual bool finalize();
+
+  protected:
+    
+
+    size_t total_events;
+    size_t kept_events;
+    
+  };
+}
+#endif
+
+//**************************************************************************
+// 
+// For Analysis framework documentation, read Manual.pdf here:
+//
+// http://microboone-docdb.fnal.gov:8080/cgi-bin/ShowDocument?docid=3183
+//
+//**************************************************************************
+
+/** @} */ // end of doxygen group 
