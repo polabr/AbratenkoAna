fc19103b6fbec0bf191336a1be89461219d5d1ff
Polina Abratenko <polabr@umich.edu>
2016-07-06 11:53:23 -0400

    Working version of TTree creator file for BNB + Cosmics. Note: doesn't account for direction yet (output is off).

--- LArSoftImport/TestMultiScatterMomentum.cxx
+++ LArSoftImport/TestMultiScatterMomentum.cxx
@@ -1,3 +1,8 @@
+// Code for creating a TTree of BNB + cosmic data
+// Filters out cosmics; matches 1 mctrack to 1 track 
+//
+// Author: Polina Abratenko
+
 #ifndef LARLITE_TESTMULTISCATTERMOMENTUM_CXX
 #define LARLITE_TESTMULTISCATTERMOMENTUM_CXX
 
@@ -6,11 +11,12 @@
 #include "DataFormat/mctruth.h"
 
 namespace larlite {
-
+  
   bool TestMultiScatterMomentum::initialize() {
 
     _tmc = TrackMomentumCalculator();
 
+    // Create TTree variables
     if (!_ana_tree) {
       _ana_tree = new TTree("ana_tree", "ana_tree");
       _ana_tree->Branch("true_mom", &_true_mom, "true_mom/D");
@@ -33,36 +39,39 @@ namespace larlite {
 		    ::larutil::Geometry::GetME()->DetLength() - fidvol_dist);
     
     return true;
+    
   }
-
+  
   bool TestMultiScatterMomentum::analyze(storage_manager* storage) {
     
+    // Set initial values for all vars
     _true_mom = -999.;
     _mcs_reco_mom = -999.;
     _true_length = -999.;
     _reco_length = -999.;
     _mu_contained = false;
 
-    std::cout << "Starting mc truth now" << "\n";
-
+    std::cout << "Starting mc truth now." << "\n";
+    
     // Get neutrino interaction vertex
-    auto ev_mctruth=storage->get_data<event_mctruth>("generator");
+    auto ev_mctruth = storage->get_data<event_mctruth>("generator");
 
+    // Neutrino vertex has to be at a single point
     if(ev_mctruth->size() != 1) 
       return false;
     
-    auto mctruth=ev_mctruth->at(0);
+    auto mctruth = ev_mctruth->at(0);
     
-    auto const& nu_vtx=mctruth.GetNeutrino().Nu().Trajectory().front().Position().Vect();
+    auto const& nu_vtx = mctruth.GetNeutrino().Nu().Trajectory().front().Position().Vect();
     
-    if (_fidvolBox.Contain(nu_vtx)){
+    if (_fidvolBox.Contain(nu_vtx)) {
       
       std::cout << "Neutrino vertex contained!" << "\n";
       
-      std::cout << "The x coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().X() << ".\n";
-      std::cout << "The y coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().Y() << ".\n";
-      std::cout << "The z coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().Z() << ".\n";	
-      
+      std::cout << "The x-coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().X() << ".\n";
+      std::cout << "The y-coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().Y() << ".\n";
+      std::cout << "The z-coordinate of the nu vertex is located at: " << mctruth.GetNeutrino().Nu().Trajectory().front().Z() << ".\n";	
+
     } else {
       print(larlite::msg::kERROR, __FUNCTION__, Form("NEUTRINO VERTEX NOT CONTAINED"));
       return false;
@@ -79,168 +88,155 @@ namespace larlite {
     std::cout << "I see " << ev_mctrack->size() << " mctracks in this event\n";
 
     // Narrowing down the number of mctracks
-    for (size_t i=0; i<ev_mctrack->size(); i++) {
+    for (size_t i = 0; i < ev_mctrack->size(); i++) {
       
-      auto mctrack=ev_mctrack->at(i);
+      auto mctrack = ev_mctrack->at(i);
       
-      if(mctrack.PdgCode()==13 && mctrack.Origin()==1) {
+      if(mctrack.PdgCode() == 13 && mctrack.Origin() == 1) {
 	
-	std::cout<<"On mctrack " << i << ", and the particle's mother PDG is "<<mctrack.MotherPdgCode()<< ", the PDG code is " << mctrack.PdgCode() << ", and the ancestor PDG is "<<mctrack.AncestorPdgCode() <<"\n";
+	std::cout << "On mctrack " << i << ", and the particle's mother PDG is " << mctrack.MotherPdgCode() << ", the PDG code is " << mctrack.PdgCode() << ", and the ancestor PDG is " << mctrack.AncestorPdgCode() << "\n";
 	std::cout << "The origin is: " << mctrack.Origin() << ".\n";
 	
-	if (mctrack.size()!=0) {
+	if (mctrack.size() != 0) {
 	  
-	  std::cout << "The x coordinate of the starting point of the muon is: " << mctrack.front().X() << ".\n"; 
-	  std::cout << "The y coordinate of the starting point of the muon is: " << mctrack.front().Y() << ".\n"; 
-	  std::cout << "The z coordinate of the starting point of the muon is: " << mctrack.front().Z() << ".\n"; 
-
+	  std::cout << "The x coordinate of the starting point of the muon is: " << mctrack.front().X() << "\n"; 
+	  std::cout << "The y coordinate of the starting point of the muon is: " << mctrack.front().Y() << "\n"; 
+	  std::cout << "The z coordinate of the starting point of the muon is: " << mctrack.front().Z() << "\n"; 
+	  
 	} else {
 	  print(larlite::msg::kERROR, __FUNCTION__, Form("mctrack.size() was 0 here!"));
-	  return false;
+	  continue;
 	}
 	
 	auto const& mct_start = mctrack.front().Position().Vect();
 	
-	double dist=(nu_vtx-mct_start).Mag();
+	double dist = (nu_vtx - mct_start).Mag();
 	
 	std::cout << "This is the difference between coordinates: " << dist << "\n";
-
-	if (dist>0.001) {
-	  
+	
+	if (dist > 0.001) {
 	  print(larlite::msg::kERROR, __FUNCTION__, Form("Distance was too large, skipping this mctrack..."));
 	  return false;
-	  
 	}
-
-	// Put here?
-	//	  W00T, next step! This is where another if statement for sorting the mctracks would go, 
-	//        after sifting through only PGD=13 and Origin=1
 	
 	// Extract MC TTree info from the one MCTrack  
+	_true_mom = mctrack.front().Momentum().Vect().Mag() / 1000.;
+	std::cout << "True mom for this mctrack: " << _true_mom << " GeV \n";
+	
+	_true_length = (mctrack.End().Position().Vect() - mctrack.Start().Position().Vect()).Mag(); 
+	std::cout << "True length for this mctrack: " << _true_length << " cm \n";	
+	
+	if (_using_mctracks) {
+	  _mcs_reco_mom = _tmc.GetMomentumMultiScatterLLHD(mctrack);
+       
+	std::cout << "Printing mcs reco mom: " << _mcs_reco_mom << " GeV \n";    
+	
+	}
 
-	_true_mom=mctrack.front().Momentum().Vect().Mag()/1000.;
-
-	_true_length=(mctrack.End().Position().Vect()-mctrack.Start().Position().Vect()).Mag(); 
-
-	if (_using_mctracks)                                                                                    
-	  _mcs_reco_mom = _tmc.GetMomentumMultiScatterLLHD(mctrack);                                                
 	std::cout << "Everything went ok so far. Now onto the Reco Tracks." << "\n";    
-
+	
 	if (!_using_mctracks) {
 	  
 	  //Get the Reco Tracks
-	  auto ev_track=storage->get_data<event_track>("pandoraNuKHit");
+	  auto ev_track = storage->get_data<event_track>("pandoraNuKHit");
 	  
-	  if (!ev_track) {
+	  if (!ev_track) {	    
 	    print(larlite::msg::kERROR, __FUNCTION__, Form("Did not find specified data product, track!"));
 	    return false;
-	  }
-
+	  } 
+	  
 	  std::cout << "I see " << ev_track->size() << " tracks in this event\n";
 	  
+	  if (ev_track->size() < 1) {
+	    print(larlite::msg::kERROR, __FUNCTION__, Form("No tracks in this event! Skipping..."));
+	    return false;
+	  }
+
+	  // Setting the final chosen track's distance to some absurdly high number to start with
+	  double minDist = 10000;
+
+	  // Just assigning a dummy value to chosenTrack here
+	  auto chosenTrack = ev_track->at(0);
+
+	  // This will be the index of the final chosen track
+	  int index = 0;
+
 	  // Sorting through the tracks
-	  for (size_t i=0; i<ev_track->size(); i++) {
+	  for (size_t i = 0; i < ev_track->size(); i++) {
 	    
-	    auto track=ev_track->at(i);
+	    auto track = ev_track->at(i);
 	    
-	    std::cout << i << "\n";
+	    auto const& trk_start = track.LocationAtPoint(0);  
+	    
+	    std::cout << "These are TRACK " << i << "'s starting X, Y, Z coordinates: " << trk_start.X() << ", " << trk_start.Y() << ", " << trk_start.Z() << "\n";
+	    
+	    auto  distBetweenStarts = sqrt(pow(trk_start.X() - mct_start.X(), 2) + pow(trk_start.Y() - mct_start.Y(), 2) + pow(trk_start.Z() - mct_start.Z(), 2));
+	    
+	    std::cout << "Dist between starts: " << distBetweenStarts << "\n";
+	    
+	    if (distBetweenStarts < minDist) {
+	      
+	      minDist = distBetweenStarts;
 
-	    auto const& trk_start = track.front().Position().Vect();
-	    auto const& trk_end = track.back().Position().Vect();
-	    
-	    std::cout << "This is this track's x coordinate: " << track.front().X() << "\n";
-	    std::cout << "This is this track's y coordinate: " << track.front().Y() << "\n";
-	    std::cout << "This is this track's z coordinate: " << track.front().Z() << "\n";
+	      chosenTrack = track;
 
+	      index = i;
+	    
+	    }
+	    
 	  }
 	  
-	  // Note: mct_start variable aleady initialized above
-	  auto const& mct_end = mctrack.back().Position().Vect();
+	  std::cout << "This is the resulting minDist: " << minDist << "\n";
+	  
+	  auto const& cho_trk_start = chosenTrack.LocationAtPoint(0);
+	  
+	  std::cout << "These are the CHOSEN TRACK's starting X, Y, Z coordinates: " << cho_trk_start.X() << ", " << cho_trk_start.Y() << ", " << cho_trk_start.Z() << "\n";
 
-	  if (ev_track->size() != 1) {
-	    print(larlite::msg::kERROR, __FUNCTION__, Form("NOTE: ev_track->size()!=1"));
-	    return false;
-	    
-	  }                    
+	  std::cout << "It is track number " << index << "\n";
+	 
+	  // Extract Reco TTree info from the one track  
+	  _mcs_reco_mom = _tmc.GetMomentumMultiScatterLLHD(chosenTrack);
 	  
-	} else {
+	  std::cout << "This is the track's mcs reco: " << _mcs_reco_mom << " GeV \n";
 	  
-	  print(larlite::msg::kERROR, __FUNCTION__, Form("These mctracks did not have the PDG=13 and Origin=1"));
+	  _reco_length = chosenTrack.Length();
 	  
-	  continue;
-	  
+	  std::cout << "This is the track's reco length: " << _reco_length << " cm \n";
+
 	}
+	
+      } else {
+	
+	print(larlite::msg::kERROR, __FUNCTION__, Form("These mctracks did not have the PDG=13 and Origin=1"));
+	continue;
 	
       }
       
-    } // Ends the for loop above.
-    
-      // MC Reco stuff should be in here
-
-    /*
-
-    // Extract MC TTree info from the one MCTrack
-    auto const& mct = ev_mctrack->at(0);
-    
-        _true_mom=mct.front().Momentum().Vect().Mag()/1000.;
-    
-     _true_length=(mct.End().Position().Vect()-mct.Start().Position().Vect()).Mag();
-    
-    if (_using_mctracks)
-      _mcs_reco_mom = _tmc.GetMomentumMultiScatterLLHD(mct);
-  
-    std::cout << "Everything went ok so far. Now onto the Reco Tracks." << "\n";
-  
-    if (!_using_mctracks) {
-
-      //Get the Reco Tracks
-      auto ev_track = storage->get_data<event_track>("pandoraNuKHit");
-          
-      if (!ev_track) {
-
-	print(larlite::msg::kERROR, __FUNCTION__, Form("Did not find specified data product, track!"));
-	return false;
-
-      }
-
-      std::cout << "I see " << ev_track->size() << " tracks in this event\n";
-
-      if (ev_track->size() != 1) {
-	print(larlite::msg::kERROR, __FUNCTION__, Form("NOTE: ev_track->size()!=1"));
-	return false;
-    
-      }
-    
-      // Extract Reco TTree info from the one track
-      auto const& trk = ev_track->at(0);
-      _mcs_reco_mom = _tmc.GetMomentumMultiScatterLLHD(trk);
-      _reco_length = trk.Length();
-  
+      _mu_contained = _fidvolBox.Contain(mctrack.front().Position().Vect()) &&  _fidvolBox.Contain(mctrack.back().Position().Vect());                                                                                    
+      
+      _ana_tree->Fill();
+      
     }
-  
-    _mu_contained = _fidvolBox.Contain(mct.front().Position().Vect()) &&
-      _fidvolBox.Contain(mct.back().Position().Vect());
-  
-    _ana_tree->Fill();
     
-    */
-
     return true;
-}
-
+    
+  }
+  
   bool TestMultiScatterMomentum::finalize() {
-  
+    
+    std::cout << "Writing ana_tree" << "\n";
     if (_fout) { _fout->cd(); _ana_tree->Write(); }
-  
+    
     else
       print(larlite::msg::kERROR, __FUNCTION__, "Did not find an output file pointer!!! File not opened?");
-  
+    
     if (_ana_tree)
       delete _ana_tree;
-  
+    
     return true;
+    
   }
-
+  
 }
 #endif
 
